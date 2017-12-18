041207e77ea419e3c72da96aabd2577be197b6e0
dddge <dddge@outlook.com>
2017-12-18 11:18:13 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 npm run build
 cd dist
-git init
+#git init
 git add -A
 git commit -m 'deploy'
-git push -f https://github.com/sinchang/sublog.git master:gh-pages
+git push -f https://github.com/dddge/dddge.github.io.git master:master
--- src/config.js
+++ src/config.js
@@ -1,26 +1,11 @@
 export default {
-  repo: 'blog', // required
-  owner: 'fouber', // required
-  title: 'fouber blog',
-  token: '', // required. split your access_token with #, create in https://github.com/settings/tokens
+  repo: 'dddge.github.io', // required
+  owner: 'dddge', // required
+  title: 'Learning By Target',
+  token: '#9a0435e9773e44bc00ae634e10ec42dc6473c443', // required. split your access_token with #, create in https://github.com/settings/tokens
   perPage: '15',
   social: [{
     name: 'github',
-    url: ''
-  }, {
-    name: 'weibo',
-    url: ''
-  }, {
-    name: 'twitter',
-    url: ''
-  }, {
-    name: 'wechat',
-    url: ''
-  }, {
-    name: 'instagram',
-    url: ''
-  }, {
-    name: 'facebook',
-    url: ''
+    url: 'https://github.com/dddge'
   }]
 }

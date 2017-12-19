9ee8db95c16f609cfed7a2adba49a2abe39b3554
dddge <dddge@outlook.com>
2017-12-19 09:54:43 +0800

    It's mine

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 npm run build
 cd dist
-#git init
+git init
+git config user.name dddge
+git config user.email dddge@outlook.com
 git add -A
 git commit -m 'deploy'
 git push -f https://github.com/dddge/dddge.github.io.git master:master
--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ export default {
   repo: 'dddge.github.io', // required
   owner: 'dddge', // required
   title: 'Learning By Target',
-  token: '#9a0435e9773e44bc00ae634e10ec42dc6473c443', // required. split your access_token with #, create in https://github.com/settings/tokens
+  token: 'e#8d056cee671bc9419087d915685a6e636485750', // required. split your access_token with #, create in https://github.com/settings/tokens
   perPage: '15',
   social: [{
     name: 'github',
